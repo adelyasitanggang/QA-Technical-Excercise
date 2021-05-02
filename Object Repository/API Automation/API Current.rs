85e76caedb6b7707e609b1f379cd7b779a6ec3b0
Adelya <Adelya@DESKTOP-F982K4R>
2021-05-02 00:23:16 -0700

    QA-Tech-Excercise

---
+++ Object Repository/API Automation/API Current.rs
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>API Current</name>
+   <tag></tag>
+   <elementGuidId>c007e117-b859-4573-82d4-fc2eeb00f0d4</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>7.9.1</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://api.weatherbit.io/v2.0/current?lat=40.730610&amp;lon=-73.935242&amp;key=711ce5976ac64e298da194e7bddcde4a&amp;include=minutely</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# QA-Technical-Exercise
+# QA-Technical-Excercise
+
+How to test with Katalon Studio :
+1. Install Node.js
+	Check if node.js is already installed:
+	node -v
+	node --version
+	npm -v
+	npm --version
+
+	Install node.js: https://nodejs.org/en/download/
+2. Install Appium
+	Check Java jdk:
+	java --version
+	java -version
+	javac -version	
+	
+	Install Appium :
+	npm install -g appium
+	
+	Check appium:
+	appium --version
+	appium -v
+	where appium
+3. Setup Android Device (Mobile) for Automation
+4. Connection and Validation
+5. Run Test Case
+
+Running Test Case :
+1. Execution Profile Default 
+2. Choose Browser (ChromeFirefox)
+
+Running API :
+1. Go to Object Folder
+2. API Automation Folder
+3. Choose Web Service Request
+4. Test Request and Verify (RUN)
